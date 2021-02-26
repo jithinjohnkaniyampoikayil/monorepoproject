e52736a5c15415a14f7345a5167b982bf3d018e3
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2021-02-26 14:50:56 +0000

    feat(node api): Node api implemented

--- server/node/testapiapp/routes/expense.js
+++ server/node/testapiapp/routes/expense.js
@@ -2,32 +2,38 @@ var express = require("express");
 const csv = require("csv-parser");
 const fs = require("fs");
 var router = express.Router();
+
+/* GET Expense  */
 let expense = [];
-/* GET home page. */
 router.get("/", function (req, res, next) {
+  expense = [];
   fs.createReadStream("public/files/expense.csv")
     .pipe(csv())
     .on("data", (row) => {
-      expense.push(row);
+      expense.push({
+        date: new Date(row.Date.split("/").reverse().join("/")),
+        category: row.Category,
+        description: row.Description,
+        amount: parseFloat(row.Amount),
+      });
     })
     .on("end", () => {
       console.log("Reading complete");
       console.log(expense.length);
-      var helper = {};
-      var groupedExpense = expense.reduce(function (r, o) {
-        var key = o.Category;
+      // var helper = {};
+      // var groupedExpense = expense.reduce(function (r, o) {
+      //   var key = o.category;
 
-        if (!helper[key]) {
-          helper[key] = Object.assign({}, o); // create a copy of o
-          helper[key].Amount = parseFloat(helper[key].Amount);
-          r.push(helper[key]);
-        } else {
-          helper[key].Amount += parseFloat(o.Amount);
-        }
-        return r;
-      }, []);
-      expense = [];
-      return res.json(groupedExpense);
+      //   if (!helper[key]) {
+      //     helper[key] = Object.assign({}, o); // create a copy of o
+      //     helper[key].amount = parseFloat(helper[key].amount);
+      //     r.push(helper[key]);
+      //   } else {
+      //     helper[key].amount += parseFloat(o.amount);
+      //   }
+      //   return r;
+      // }, []);
+      return res.json(expense);
     });
 });
 
--- ui/ExpenseTracker/src/environments/environment.ts
+++ ui/ExpenseTracker/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://my-prod-url'
+  apiUrl: 'http://localhost:3000/expense',
+  isCsvReadMode: false,
 };
 
 /*
